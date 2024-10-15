976946356d6cee251676a4b7fcf51c8811da2449
Rodrigo <rcarmona@realmetrics.io>
2024-10-15 00:11:21 -0600

    test controller

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request, "core/home.html")
 
 def about(request):
-    print("asdsdf")
+    print("asdd")
     return render(request,"core/about.html")
 
 
--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -4,7 +4,7 @@ from .models import Project
 # Create your views here.
 
 def portfolio(request):
-    print("another archaasdaive")
+    print("another achaasdive")
     projects = Project.objects.all()
     return render(request,"portfolio/portfolio.html", {'projects':projects})
 
